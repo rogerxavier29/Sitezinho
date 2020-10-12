c46a615963d8e66ff2ccc054d06af401b48a5a0f
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2020-10-12 16:15:04 -0300

    declarando variaveis e passando no metodo post + pegando variaveis la do html

--- site-roger/email.php
+++ site-roger/email.php
@@ -1,6 +1,11 @@
 <?php
 
 
+$nome = addcslashes($_POST['name'])
+$email = addcslashes($_POST['email'])
+$assunto = addcslashes($_POST['assunto'])
+$mensagem = addslashes($_POST['message'])
+
 
 
 ?>

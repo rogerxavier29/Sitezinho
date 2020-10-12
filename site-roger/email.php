63582669c47a25dcb860ce442d34fc5b71b1eefe
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2020-10-12 16:27:14 -0300

    verificao do email

--- site-roger/email.php
+++ site-roger/email.php
@@ -1,11 +1,12 @@
 <?php
 
+if(isset($_POST['email']) && !empty($_POST['email'])){
 
 $nome = addcslashes($_POST['name'])
 $email = addcslashes($_POST['email'])
 $assunto = addcslashes($_POST['assunto'])
 $mensagem = addslashes($_POST['message'])
 
-
+}
 
 ?>
